7ba519504b10b4b96aa532860601596023fccc6b
Jason Kwan <j-kwan@outlook.com>
2022-04-09 23:01:29 -0400

    added gif functionality

--- kittengacha/kittengacha/PullConversionView.swift
+++ kittengacha/kittengacha/PullConversionView.swift
@@ -9,43 +9,53 @@ import Foundation
 import SwiftUI
 import UIKit
 
-class ViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Create image
-        let image = UIImage(named: "button.png")
-        
-        let button = UIButton()
-        button.frame = CGRect(x: 10, y: 100, width: 100, height: 100)
-        button.setBackgroundImage(image, for: UIControl.State.normal)
-        button.addTarget(self, action:#selector(self.imageButtonTapped(_:)), for: .touchUpInside)
-        
-        self.view.addSubview(button)
-        
-    }
-    
-    @objc func imageButtonTapped(_ sender:UIButton!)
-    {
-        print("My image button tapped")
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-}
+
 
 struct PullConversionView: View {
-    var totalSteps = 0
+    
+    @State private var speed: Double = 0
+    @State private var isEditing = false
     
     @State var test: String = ""
     @State var pc: String = ""
+    
     var cat = Cats()
+    
     @ObservedObject var screenNumber: Screens
     
     var body: some View {
+        GifImage("crying")
+        HStack {
+            Image("foot")
+                .resizable()
+                .frame(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height / 15)
+            Image(systemName: "arrow.forward")
+                .font(.system(size: 56.0))
+            Image("paw print")
+                .resizable()
+                .frame(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height / 5)
+        }
         VStack {
+            Spacer()
+            
+            HStack {
+                Slider(value: $speed, in: 0...screenNumber.userStats.steps/2000 + 1, step: 1) {
+                        Text("\(speed)")
+                            .foregroundColor(isEditing ? .red : .blue)
+                    }
+                    minimumValueLabel: {
+                        Text("0")
+                    } maximumValueLabel: {
+                        Text(String(screenNumber.userStats.steps/2000))
+                    }
+                    onEditingChanged: { editing in
+                        isEditing = editing
+                    }
+
+            }
+            .padding(.leading)
+            .padding(.trailing)
+            
             Button(action: {
             }, label: {
                 
@@ -55,12 +65,14 @@ struct PullConversionView: View {
                         .frame(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height / 15)
                     Text("Convert Steps")
                         .foregroundColor(.red)
+
                 }
             })
+        Spacer()
+        Text("You have ")
         .padding(.top)
         Text(pc)
         Text(test)
-        Spacer()
         Image(test)
             .resizable()
             .scaledToFit()
