54cc3dd39afcd885caf94ebb8a04bb0b8c42e453
Jason Kwan <j-kwan@outlook.com>
2022-04-09 23:43:44 -0400

    update

--- kittengacha/kittengacha/PullConversionView.swift
+++ kittengacha/kittengacha/PullConversionView.swift
@@ -24,22 +24,28 @@ struct PullConversionView: View {
     @ObservedObject var screenNumber: Screens
     
     var body: some View {
-        GifImage("crying")
-        HStack {
-            Image("foot")
-                .resizable()
-                .frame(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height / 15)
-            Image(systemName: "arrow.forward")
-                .font(.system(size: 56.0))
-            Image("paw print")
-                .resizable()
-                .frame(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height / 5)
-        }
         VStack {
             Spacer()
-            
-            HStack {
-                Slider(value: $speed, in: 0...screenNumber.userStats.steps/2000 + 1, step: 1) {
+            if (Int(screenNumber.userStats.steps / 2000 ) == 0) {
+                GifImage("crying")
+                    .offset(y: UIScreen.main.bounds.height / 4)
+                Text("You do not have enough steps to convert into Paw Prints")
+                    .foregroundColor(.cyan)
+                    .padding(.bottom, UIScreen.main.bounds.height / 8)
+            }
+            else {
+                HStack {
+                    Image("foot")
+                        .resizable()
+                        .frame(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height / 15)
+                    Image(systemName: "arrow.forward")
+                        .font(.system(size: 56.0))
+                    Image("paw print")
+                        .resizable()
+                        .frame(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height / 5)
+                }
+                VStack {
+                    Slider(value: $speed, in: 0...screenNumber.userStats.steps/2000 + 1, step: 1) {
                         Text("\(speed)")
                             .foregroundColor(isEditing ? .red : .blue)
                     }
@@ -49,16 +55,15 @@ struct PullConversionView: View {
                         Text(String(screenNumber.userStats.steps/2000))
                     }
                     onEditingChanged: { editing in
-                        isEditing = editing
-                    }
-
-            }
-            .padding(.leading)
-            .padding(.trailing)
-            
-            Button(action: {
-            }, label: {
+                    isEditing = editing
+                }
+                .padding(.leading)
+                .padding(.trailing)
                 
+                Button("Dec") {
+                    screenNumber.userStats.steps -= 2000 * speed
+                }
+                    
                 ZStack {
                     Image("button")
                         .resizable()
@@ -66,16 +71,16 @@ struct PullConversionView: View {
                     Text("Convert Steps")
                         .foregroundColor(.red)
 
+                        }
+            
+                .padding(.top)
+                Text(pc)
+                Text(test)
+                Image(test)
+                    .resizable()
+                    .scaledToFit()
                 }
-            })
-        Spacer()
-        Text("You have ")
-        .padding(.top)
-        Text(pc)
-        Text(test)
-        Image(test)
-            .resizable()
-            .scaledToFit()
+            }
         }
     }
 }
