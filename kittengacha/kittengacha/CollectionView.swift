a1bb70ffd4dbb2dc22ce7c2f4bfb91e4ed0ef2ea
Jason Kwan <j-kwan@outlook.com>
2022-04-10 01:50:28 -0400

    collectionGrid

--- kittengacha/kittengacha/CollectionView.swift
+++ kittengacha/kittengacha/CollectionView.swift
@@ -10,15 +10,37 @@ import SwiftUI
 
 struct CollectionView: View {
     @ObservedObject var screenNumber: Screens
-    
-    var body: some View {
-        ScrollView(.vertical, showsIndicators: true) {
-            ForEach(0..<screenNumber.userStats.collection.count) { i in
-                Text("\(screenNumber.userStats.collection[i])")
-            }
-        }
-        Button("Pull") {
-            screenNumber.screenNumber = 0
-        }
+  var body: some View {
+    VStack(spacing: -1.0)
+    {
+      HStack(spacing: -1.0)
+      {
+        Tile()
+          .border(Color.red, width: 0.90)
+        Tile()
+          .border(Color.red, width: 0.90)
+      }
+      HStack(spacing: -1.0)
+      {
+        Tile()
+        .border(Color.red, width: 0.90)
+        Tile()
+          .border(Color.red, width: 0.90)
+      }
+      .padding(.bottom, 15)
+    }
+  }
+}
+struct Tile : View
+{
+    var body: some View
+    {
+        VStack
+        {
+            Spacer()
+            Text("Test")
+            Spacer()
+        }.frame(width: 150, height: 150)
     }
 }
+
